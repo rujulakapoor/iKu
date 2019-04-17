a0b5ec9f5b5f89fe1983b2c180efdd67feb8209f
Jeonghu <park955@purdue.edu>
2019-04-17 12:47:04 -0400

    Logo changed and subtitle added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App ;)"/>
+    <img alt="Vue logo" src="./assets/iKu_cropped.png">
+    <HelloWorld msg="Share your Haiku"/>
+    
   </div>
 </template>
 
