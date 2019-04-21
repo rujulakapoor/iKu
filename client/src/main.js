b714c2333fac67234fbe8bf0e78858d7cbc9c02b
Jeonghu <park955@purdue.edu>
2019-04-20 20:48:35 -0400

    Syllable counting working

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,14 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import GoogleLogin from 'vue-google-login';
+import VueResource from 'vue-resource';
 
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.component('GoogleLogin', GoogleLogin);
+Vue.use(VueResource);
 
-Vue.config.productionTip = false
-Vue.use(BootstrapVue)
-Vue.component('GoogleLogin', GoogleLogin)
+Vue.http.headers.common['Access-Control-Allow-Origin'] = '*';
 
 new Vue({
   render: h => h(App),
